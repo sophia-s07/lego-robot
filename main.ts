c86389cb9c29b7994e316fcfbb9176551e6f7592
sophia-s07 <133241675+sophia-s07@users.noreply.github.com>
2023-05-11 11:28:58 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         wuKong.setMotorSpeed(wuKong.MotorList.M1, 100)
+    } else {
+        wuKong.stopMotor(wuKong.MotorList.M1)
     }
 })
 input.onButtonPressed(Button.A, function () {
